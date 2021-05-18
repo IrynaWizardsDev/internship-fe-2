88456251c9e432d21c252f522b33a54cf6d48e7d
IrynaWizardsDev <iryna@wizardsdev.com>
2021-05-18 09:21:44 +0300

    deployment fix - add firebase/config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyC2OrWgDbXXPzzik-s4VvH5ezLRSiGawNw',
+  authDomain: 'hotel-da9e2.firebaseapp.com',
+  databaseURL: 'https://hotel-da9e2-default-rtdb.firebaseio.com',
+  projectId: 'hotel-da9e2',
+  storageBucket: 'hotel-da9e2.appspot.com',
+  messagingSenderId: '653089467063',
+  appId: '1:653089467063:web:60e9ea13e32a80da7dfb32',
+};
